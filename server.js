995e0d8c5422b780e56ff4ca91577fe8021463dc
KINAN WHYTE <kinanwhyte@gmail.com>
2017-01-24 15:45:59 -0700

    Link folder form to server

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const reload = require('reload')
 const express = require('express')
 const app = express()
-const indexTemplate = require('./templates')
+const md5 = require('md5')
+
+const indexTemplate = require('./template')
 
 app.set('port', process.env.PORT || 3000)
+
+app.use(express.static('./public'))
 
 app.get("/", (req, res) => {
   res.send(indexTemplate)
--- templates/index.js
+++
@@ -1,5 +0,0 @@
-const indexTemplate = `
-  <h1>Jet Fuel</h1>
-  <script src='reload/reload.js'></script>
-`
-module.exports = indexTemplate
