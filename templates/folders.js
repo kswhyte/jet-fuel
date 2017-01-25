b24a2f2b9049d701a94e2ee332d9c04f78297086
Hilary Lewis <hilarylewis92@gmail.com>
2017-01-25 16:05:12 -0700

    Folders list function made into reduce

--- templates/folders.js
+++ templates/folders.js
@@ -1,9 +1,9 @@
 const foldersList = (folders = {}) => {
   let folderKeys = Object.keys(folders)
 
-  return folderKeys.map(folderID => {
-    return `<li>${folders[folderID]}<li>`
-  })
+  return folderKeys.reduce((str, folderID) => {
+    return str += `<li>${folders[folderID]}</li>`
+  }, '')
 }
 
 module.exports = foldersList
