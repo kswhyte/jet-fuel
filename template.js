f220224921077f7d940eb35e3d465c1a6a0b06ad
KINAN WHYTE <kinanwhyte@gmail.com>
2017-01-25 16:51:16 -0700

    Add icon for app, polish button and input fields

--- template.js
+++ template.js
@@ -3,12 +3,13 @@ const indexTemplate = `
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>Jet Fuel</title>
+    <title>Jet<span class="fuel-highlight"> Fuel</span></title>
     <link rel="stylesheet" type="text/css" href="./styles.css">
     <link href="https://fonts.googleapis.com/css?family=Arsenal|Lato|Open+Sans" rel="stylesheet">
   </head>
   <body>
     <h1 class="title">Jet Fuel</h1>
+    <img class="rocket-ship" src="./icons/transport-2.svg" alt="rocket ship">
     <form>
       <label for="folder-label">Add a new folder to house similar URLs:</label>
       <input id="folder-input" placeholder="Full URL folder..."></input>
